adbae0b46bc74af7bf14020e6f2b3f6555a6e669
Kai McG <gmac283@gmail.com>
2024-07-15 12:35:19 +0100

    Add files via upload

---
+++ Sorts and Searches/Bubble+Insert Sort/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.BtnPopulate = New System.Windows.Forms.Button()
+        Me.BtnBubbleSort = New System.Windows.Forms.Button()
+        Me.LstOutput = New System.Windows.Forms.ListBox()
+        Me.BtnInsertSort = New System.Windows.Forms.Button()
+        Me.BtnClear = New System.Windows.Forms.Button()
+        Me.BtnBinarySearch = New System.Windows.Forms.Button()
+        Me.BtnLinearSearch = New System.Windows.Forms.Button()
+        Me.BtnQuickSort = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BtnPopulate
+        '
+        Me.BtnPopulate.Location = New System.Drawing.Point(12, 12)
+        Me.BtnPopulate.Name = "BtnPopulate"
+        Me.BtnPopulate.Size = New System.Drawing.Size(199, 96)
+        Me.BtnPopulate.TabIndex = 0
+        Me.BtnPopulate.Text = "Populate Array"
+        Me.BtnPopulate.UseVisualStyleBackColor = True
+        '
+        'BtnBubbleSort
+        '
+        Me.BtnBubbleSort.Location = New System.Drawing.Point(12, 114)
+        Me.BtnBubbleSort.Name = "BtnBubbleSort"
+        Me.BtnBubbleSort.Size = New System.Drawing.Size(199, 96)
+        Me.BtnBubbleSort.TabIndex = 1
+        Me.BtnBubbleSort.Text = "Bubble Sort"
+        Me.BtnBubbleSort.UseVisualStyleBackColor = True
+        '
+        'LstOutput
+        '
+        Me.LstOutput.FormattingEnabled = True
+        Me.LstOutput.Location = New System.Drawing.Point(218, 13)
+        Me.LstOutput.Name = "LstOutput"
+        Me.LstOutput.Size = New System.Drawing.Size(298, 290)
+        Me.LstOutput.TabIndex = 2
+        '
+        'BtnInsertSort
+        '
+        Me.BtnInsertSort.Location = New System.Drawing.Point(12, 216)
+        Me.BtnInsertSort.Name = "BtnInsertSort"
+        Me.BtnInsertSort.Size = New System.Drawing.Size(199, 96)
+        Me.BtnInsertSort.TabIndex = 3
+        Me.BtnInsertSort.Text = "Insertion Sort"
+        Me.BtnInsertSort.UseVisualStyleBackColor = True
+        '
+        'BtnClear
+        '
+        Me.BtnClear.Location = New System.Drawing.Point(522, 216)
+        Me.BtnClear.Name = "BtnClear"
+        Me.BtnClear.Size = New System.Drawing.Size(199, 96)
+        Me.BtnClear.TabIndex = 4
+        Me.BtnClear.Text = "Clear Box"
+        Me.BtnClear.UseVisualStyleBackColor = True
+        '
+        'BtnBinarySearch
+        '
+        Me.BtnBinarySearch.Location = New System.Drawing.Point(522, 12)
+        Me.BtnBinarySearch.Name = "BtnBinarySearch"
+        Me.BtnBinarySearch.Size = New System.Drawing.Size(199, 96)
+        Me.BtnBinarySearch.TabIndex = 5
+        Me.BtnBinarySearch.Text = "Binary Search"
+        Me.BtnBinarySearch.UseVisualStyleBackColor = True
+        '
+        'BtnLinearSearch
+        '
+        Me.BtnLinearSearch.Location = New System.Drawing.Point(522, 114)
+        Me.BtnLinearSearch.Name = "BtnLinearSearch"
+        Me.BtnLinearSearch.Size = New System.Drawing.Size(199, 96)
+        Me.BtnLinearSearch.TabIndex = 6
+        Me.BtnLinearSearch.Text = "Linear Search"
+        Me.BtnLinearSearch.UseVisualStyleBackColor = True
+        '
+        'BtnQuickSort
+        '
+        Me.BtnQuickSort.Location = New System.Drawing.Point(12, 318)
+        Me.BtnQuickSort.Name = "BtnQuickSort"
+        Me.BtnQuickSort.Size = New System.Drawing.Size(199, 96)
+        Me.BtnQuickSort.TabIndex = 7
+        Me.BtnQuickSort.Text = "Quick Sort"
+        Me.BtnQuickSort.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(732, 422)
+        Me.Controls.Add(Me.BtnQuickSort)
+        Me.Controls.Add(Me.BtnLinearSearch)
+        Me.Controls.Add(Me.BtnBinarySearch)
+        Me.Controls.Add(Me.BtnClear)
+        Me.Controls.Add(Me.BtnInsertSort)
+        Me.Controls.Add(Me.LstOutput)
+        Me.Controls.Add(Me.BtnBubbleSort)
+        Me.Controls.Add(Me.BtnPopulate)
+        Me.Name = "Form1"
+        Me.Text = "Sorts"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents BtnPopulate As Button
+    Friend WithEvents BtnBubbleSort As Button
+    Friend WithEvents LstOutput As ListBox
+    Friend WithEvents BtnInsertSort As Button
+    Friend WithEvents BtnClear As Button
+    Friend WithEvents BtnBinarySearch As Button
+    Friend WithEvents BtnLinearSearch As Button
+    Friend WithEvents BtnQuickSort As Button
+End Class
---
+++ Sorts and Searches/Bubble+Insert Sort/Form1.vb
@@ -0,0 +1,164 @@
+﻿Imports System.Windows.Forms.AxHost
+Imports Microsoft.VisualBasic.Logging
+
+Public Class Form1
+    Dim Array() As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim intArraySize As Integer = InputBox("How many elements?") - 1
+        ReDim Array(intArraySize)
+    End Sub
+
+    Private Sub BtnPopulate_Click(sender As Object, e As EventArgs) Handles BtnPopulate.Click
+        Randomize()
+        For i = 0 To Array.Length() - 1
+            Array(i) = Math.Floor(Rnd() * 100)
+            LstOutput.Items.Add(Array(i))
+        Next
+    End Sub
+
+    Private Sub BtnBubbleSort_Click(sender As Object, e As EventArgs) Handles BtnBubbleSort.Click
+        Dim n As Integer = Array.Length() - 1
+        Dim temp As Integer
+        Dim swapped As Boolean = True
+
+        Do Until swapped = False        'will keep doing passes until there have been no swaps
+            swapped = False
+            For i = 0 To n - 1
+                If Array(i) > Array(i + 1) Then
+                    temp = Array(i + 1)
+                    Array(i + 1) = Array(i)
+                    Array(i) = temp
+                    swapped = True
+
+                End If
+            Next
+        Loop
+        Output()
+    End Sub
+
+    Sub Output()
+        LstOutput.Items.Clear()
+        For i = 0 To Array.Length() - 1
+            LstOutput.Items.Add(Array(i))
+        Next
+    End Sub
+
+    Private Sub BtnInsertSort_Click(sender As Object, e As EventArgs) Handles BtnInsertSort.Click
+        Dim i, j, n, currentItem As Integer
+        Dim inserted As Boolean
+
+        n = UBound(Array) + 1
+
+        For i = 1 To n - 1
+            currentItem = Array(i)
+            inserted = False
+            j = i - 1
+
+            Do While (j >= 0 And inserted = False)
+                If (currentItem < Array(j)) Then
+                    Array(j + 1) = Array(j)
+                    j = j - 1
+                    Array(j + 1) = currentItem
+                Else
+                    inserted = True
+                End If
+            Loop
+        Next
+        Output()
+    End Sub
+
+    Private Sub BtnClear_Click(sender As Object, e As EventArgs) Handles BtnClear.Click
+        LstOutput.Items.Clear()
+    End Sub
+
+    Private Sub BtnBinarySearch_Click(sender As Object, e As EventArgs) Handles BtnBinarySearch.Click
+        Dim StartP, EndP, MidP, SearchValue As Integer
+        Dim Found As Boolean = False
+
+        StartP = 0
+        EndP = UBound(Array)
+
+        SearchValue = InputBox("What Value are you looking for?")
+
+        Do Until Found = True
+            MidP = (StartP + EndP) \ 2
+            If StartP > EndP Then
+                MsgBox("Item not in list")
+                Exit Do
+
+            ElseIf SearchValue = Array(MidP) Then
+                Found = True
+                MsgBox("Item found at index " & MidP + 1)
+
+            ElseIf SearchValue > Array(MidP) Then
+                StartP = MidP + 1
+
+            ElseIf SearchValue < Array(MidP) Then
+                EndP = MidP - 1
+            End If
+        Loop
+
+    End Sub
+
+    Private Sub BtnLinearSearch_Click(sender As Object, e As EventArgs) Handles BtnLinearSearch.Click
+        Dim Found As Boolean = False
+        Dim SearchValue As Integer = InputBox("What Value are you looking for?")
+
+        For i = 0 To UBound(Array)
+            If Array(i) = SearchValue Then
+                MsgBox("item found at index " & i + 1)
+                Found = True
+            End If
+        Next
+        If Found = False Then
+            MsgBox("Item not in list")
+        End If
+    End Sub
+
+    Private Sub BtnQuickSort_Click(sender As Object, e As EventArgs) Handles BtnQuickSort.Click
+        quickSortProc(Array, 0, Array.Length - 1)
+    End Sub
+
+    Sub quickSortProc(Array() As Integer, indexLow As Integer, indexHigh As Integer)
+        Dim Pivot, tempSwap, tempLow, tempHigh As Integer
+
+        tempLow = indexLow
+        tempHigh = indexHigh
+
+        Pivot = (Array((indexLow + indexHigh) / 2))
+
+        While (tempLow <= tempHigh)
+            'increment
+            While (Array(tempLow) < Pivot And tempLow < indexHigh)
+                tempLow += 1
+            End While
+
+            'decrement
+            While (Pivot < Array(tempHigh) And tempHigh > indexLow)
+                tempHigh -= 1
+            End While
+
+            'swap
+            If (tempLow <= tempHigh) Then
+                tempSwap = Array(tempLow)
+                Array(tempLow) = Array(tempHigh)
+                Array(tempHigh) = tempSwap
+
+                tempLow += 1
+                tempHigh -= 1
+
+            End If
+
+        End While
+
+        If (indexLow < tempHigh) Then
+            quickSortProc(Array, indexLow, tempHigh)
+        End If
+
+        If tempLow < indexHigh Then
+            quickSortProc(Array, tempLow, indexHigh)
+        End If
+
+        Output()
+    End Sub
+End Class
